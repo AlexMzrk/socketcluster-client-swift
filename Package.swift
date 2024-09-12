22aeba5120b8db4d05fca886c9be58241cbb22c3
Cube <cube@unitedtech.ai>
2024-09-12 14:02:38 +0200

    update dependecy

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/daltoniam/Starscream.git", exact: "3.1.2"),
-        .package(url: "https://github.com/Miles-Matheson/HandyJSON.git", branch: "master")
+        .package(url: "https://github.com/AlexMzrk/HandyJSON.git", .branch("dev"))
         ],
     targets: [
         .target(
